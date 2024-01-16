b26f8ffab20f44d7f6733958f8178995069acc0f
John Bailey <jbailey679@yahoo.com>
2024-01-16 17:07:42 -0600

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import book1 from "./images/book1.jpg";
+import book2 from "./images/book2.jpg";
+import book3 from "./images/book3.jpg";
+
+export const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: book1,
+    id: 1,
+  },
+  {
+    author: "Barbara Rieco",
+    title: "More! More! More!",
+    img: book2,
+    id: 2,
+  },
+  {
+    author: "Morgan Housel",
+    title: "The Psychology of Money",
+    img: book3,
+    id: 3,
+  },
+];
